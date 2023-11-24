ccb5597fc0c3439f257fdfff1e48da82a04535c0
igor-sb <>
2023-11-24 10:44:58 -0800

    add index unit tests

---
+++ bluprint/index.py
@@ -0,0 +1,49 @@
+"""Indexing directory contents into a YAML config."""
+
+import os
+from pathlib import Path, PosixPath
+
+from omegaconf import DictConfig, OmegaConf
+
+
+def _index_files_in_dir(
+    index_dir: str | PosixPath,
+    skip_dot_files: bool = True,
+) -> list[tuple[str, ...]]:
+    indexed_files = []
+    for root, _, files in os.walk(index_dir):
+        for file in files:  # noqa: WPS110
+            file_path = Path(root) / file
+            if file.startswith('.') and skip_dot_files:
+                continue
+            indexed_files.append(
+                file_path.parts[len(Path(index_dir).parts):],
+            )
+    return sorted(indexed_files)
+
+
+def _create_config_from_indexed_files(
+    indexed_files: list[tuple[str, ...]],
+) -> DictConfig:
+    config_dotlist = []
+    for indexed_file in indexed_files:
+        final_key = Path(indexed_file[-1]).stem.replace('.', '_')
+        nonfinal_keys = [part.replace('.', '_') for part in indexed_file[:-1]]
+        config_dotlist.append(
+            '{key}={value}'.format(
+                key='.'.join([*nonfinal_keys, final_key]),
+                value='/'.join(indexed_file),
+            ),
+        )
+    return OmegaConf.from_dotlist(config_dotlist)
+
+
+def index_dir_to_config_yaml(
+    input_dir: str | PosixPath,
+    output_yaml: str | PosixPath,
+) -> None:
+    indexed_files = _index_files_in_dir(input_dir)
+    OmegaConf.save(
+        _create_config_from_indexed_files(indexed_files),
+        output_yaml,
+    )
---
+++ tests/index/test_index.py
@@ -0,0 +1,20 @@
+"""Test directory indexing and YAML output."""
+
+import tempfile
+from pathlib import Path
+
+from bluprint.index import index_dir_to_config_yaml
+
+
+def test_index_dir_to_config_yaml(
+    snapshot,
+):
+    test_data_dir = Path('tests') / 'index' / 'fixtures'
+    expected_yaml = 'data.yaml'
+    with tempfile.NamedTemporaryFile('r+') as actual_yaml:
+        index_dir_to_config_yaml(test_data_dir, actual_yaml.name)
+        snapshot.snapshot_dir = Path('tests') / 'index' / 'snapshots'
+        snapshot.assert_match(
+            actual_yaml.read(),
+            expected_yaml,
+        )

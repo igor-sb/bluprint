22cdec1e3fbd4274d692d79856625a28b85d7b53
igor-sb <>
2023-11-25 23:04:23 -0800

    remove old logging

--- bluprint/demo.py
+++ bluprint/demo.py
@@ -1,7 +1,6 @@
 """Placeholder file contents for new projects."""
 
 import shutil
-import sys
 from importlib import resources
 from importlib.abc import Traversable
 from pathlib import Path, PosixPath
@@ -12,21 +11,17 @@ def copy_demo_files(
     project_dir: str | Path | PosixPath,
 ) -> None:
     demo_path = str(dir_in_package('demo'))
-    sys.stderr.write('Copying demo generic files:')
     shutil.copytree(
         src=Path(demo_path),
         dst=project_dir,
         ignore=shutil.ignore_patterns('project'),
         dirs_exist_ok=True,
     )
-    sys.stderr.write(' ✓\n')
-    sys.stderr.write('Copying demo project files:')
     shutil.copytree(
         src=Path(demo_path) / 'project',
         dst=Path(project_dir) / project_name,
         dirs_exist_ok=True,
     )
-    sys.stderr.write(' ✓\n')
 
 
 def dir_in_package(package_dir: str) -> PosixPath | Traversable:

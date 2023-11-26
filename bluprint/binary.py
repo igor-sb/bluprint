30054efa9b2b553c204017b0d619e89aec32e510
igor-sb <>
2023-11-25 23:05:33 -0800

    fix linting

--- bluprint/binary.py
+++ bluprint/binary.py
@@ -6,6 +6,7 @@ import shutil
 class MissingExecutableError(Exception):
     """Raises an exception if a required executable is missing."""
 
+
 def check_if_executable_is_installed(executable: str) -> None:
     if not shutil.which(executable):
         raise MissingExecutableError(f'{executable} not found.')
